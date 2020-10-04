27baa3077f22a691931ff27a7bac09dca22b50f5
EarthSiwapol <siwapol@blockfint.com>
2020-10-04 12:59:50 +0700

    add event

--- x/logistic/types/events.go
+++ x/logistic/types/events.go
@@ -2,14 +2,20 @@ package types
 
 // logistic module event types
 const (
-	// TODO: Create your event types
-	// EventType<Action>    		= "action"
+	EventTypeInitDeal  = "InitDeal"
+	EventTypeTransport = "Transport"
+	EventTypeUpdateTmp = "UpdateTmp"
+	EventTypeReceive   = "Receive"
+	EventTypeReject    = "Reject"
 
-	// TODO: Create keys fo your events, the values will be derivided from the msg
-	// AttributeKeyAddress  		= "address"
-
-	// TODO: Some events may not have values for that reason you want to emit that something happened.
-	// AttributeValueDoubleSign = "double_sign"
+	AttributeCreator     = "creator"
+	AttributeTransporter = "transporter"
+	AttributeCustomer    = "customer"
+	AttributePrice       = "price"
+	AttributeMaxTmp      = "maxTmp"
+	AttributeMinTmp      = "minTmp"
+	AttributeCancelable  = "cancelable"
+	AttributeUpdateTmp   = "updateTmp"
 
 	AttributeValueCategory = ModuleName
 )
