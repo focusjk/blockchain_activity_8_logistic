e778b3612001485890213b7f470536d1582ab76d
EarthSiwapol <siwapol@blockfint.com>
2020-10-11 15:11:18 +0700

    register handler functions to main handler

--- x/logistic/handler.go
+++ x/logistic/handler.go
@@ -1,12 +1,14 @@
+// Note: this function is mapping Msg request to the handler
+
 package logistic
 
 import (
 	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/earth2378/logistic/x/logistic/keeper"
 	"github.com/earth2378/logistic/x/logistic/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // NewHandler ...
@@ -14,7 +16,18 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 		switch msg := msg.(type) {
-    // this line is used by starport scaffolding
+		// Step 7: register handler functions to main handler
+		case types.MsgInitDeal:
+			return handlerMsgInitDeal(ctx, k, msg)
+		case types.MsgTransport:
+			return handlerMsgTransport(ctx, k, msg)
+		case types.MsgUpdateTemp:
+			return handlerMsgUpdateTemp(ctx, k, msg)
+		case types.MsgReceive:
+			return handlerMsgReceive(ctx, k, msg)
+		case types.MsgReject:
+			return handlerMsgReject(ctx, k, msg)
+
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
